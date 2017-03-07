817b0690d0d681e9f0fa53038bdcfd6dac31bd00
Aram <aramjzap@gmail.com>
2017-03-07 01:07:24 -0300

    Initial commit

---
+++ src/README.md
@@ -0,0 +1,13 @@
+# ProgAv
+Início dos estudos em programação JAVA:
+
+Os exemplos de código contidos nesse projeto são fruto da disciplina _EL83D - Programação Avançada_, ministrada no 3º 
+período do curso de Tecnologia em Sistemas de Telecomunicações da UTFPR.
+
+Os códigos estão agrupados em ordem progressiva de dificuldade e abragem conceitos importantes no aprendizado da
+linguagem de programação JAVA.
+
+Este projeto está em desenvolvimento, mas perguntas, dúvidas ou sugestões são bem-vindas.
+
+
+Desejo a todos uma boa programação!
---
+++ src/sistel/progav/_02.java
@@ -0,0 +1,25 @@
+/* Cálculo do resto da divisão por 2 de um número inteiro */
+
+/** author: Aram Zap */
+/** date: 06/03/2017 */
+
+package sistel.progav;
+
+import java.util.Scanner;
+
+public class _02{
+    public static void main(String args[]){
+        Scanner ler = new Scanner(System.in);
+
+        int num,resto;
+
+        System.out.println("Informe um numero inteiro: ");
+        num = ler.nextInt();
+
+        resto = num % 2;
+
+        System.out.println();
+
+        System.out.printf("%d resto 2 = %d\n", num, resto);
+    }
+}
