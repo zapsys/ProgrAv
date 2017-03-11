bde633343a36c45287160947c95c28bbaf9a40fa
Aram <aramjzap@gmail.com>
2017-03-10 22:32:51 -0300

    Exercício 2

---
+++ src/lista_01/_02.java
@@ -0,0 +1,16 @@
+/** author: Aram Zap */
+/** date: 10/03/2017 */
+
+package lista_01;
+
+public class _02 {
+    public static void main(String args[]) {
+
+        int a = 5, b = 6, c = 9;
+        float media;
+
+        media = (a + b + c)/(float)3;
+
+        System.out.printf("A média de %d, %d e %d é igual a %7.5f", a, b, c, media);
+    }
+}
