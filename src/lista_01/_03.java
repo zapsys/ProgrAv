ba4a438de316b31f92310e82bef8144912c013fb
Aram <aramjzap@gmail.com>
2017-03-10 22:36:08 -0300

    Exercício 3

---
+++ src/lista_01/_03.java
@@ -0,0 +1,27 @@
+/** author: Aram Zap */
+/** date: 10/03/2017 */
+
+package lista_01;
+
+import java.util.Scanner;
+
+public class _03 {
+    public static void main(String args[]) {
+        Scanner ler = new Scanner(System.in);
+
+        int a, b;
+
+        System.out.printf("Entre com o 1o. valor: ");
+        a = ler.nextInt();
+
+        System.out.printf("Entre com o 2o. valor: ");
+        b = ler.nextInt();
+
+        if (a > b)
+            System.out.printf("%d é maior que %d", a, b);
+        else if (a == b)
+            System.out.printf("%d é igual a %d", a, b);
+        else
+            System.out.printf("%d é menor que %d", a, b);
+    }
+}
