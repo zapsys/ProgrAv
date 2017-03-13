f30008e29e8e1f6384ee5c1a7bb567374a5616e8
Aram <aramjzap@gmail.com>
2017-03-13 19:11:23 -0300

    Exercício 11

---
+++ src/sistel/progav/_11.java
@@ -0,0 +1,23 @@
+/* Tabuada usando a estrutura de repetição 'for' */
+
+/** author: Aram Zap */
+/** date: 13/03/2017 */
+
+package sistel.progav;
+
+import java.util.Scanner;
+
+public class _11 {
+    public static void main(String args[]) {
+        Scanner ler = new Scanner(System.in);
+
+        int i, n;
+
+        System.out.print("Entre com um número: ");
+        n = ler.nextInt();
+
+        for (i = 1; i <= 10; i++){
+            System.out.printf("%d x %d = %d\n", i, n, i*n);
+        }
+    }
+}
