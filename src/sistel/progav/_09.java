26a47981a147a90d953b350bbaaec72be9a407aa
Aram <aramjzap@gmail.com>
2017-03-13 19:13:38 -0300

    Exercício 9

--- src/sistel/progav/_09.java
+++ src/sistel/progav/_09.java
@@ -1,12 +1,20 @@
+/* Uso da estrutura de repetição 'while' */
+
 /** author: Aram Zap */
 /** date: 13/03/2017 */
 
 package sistel.progav;
 
-import java.util.Scanner;
-
 public class _09 {
     public static void main(String args[]) {
-        Scanner ler = new Scanner(System.in);
+        int i;
+
+        i = 5;
+
+        while (i <= 50){
+            System.out.printf("%d\n",i);
+
+            i = i + 5;
+        }
     }
 }
