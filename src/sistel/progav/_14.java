3e3a635e842e2f6a66fca91fcadd8431761559de
Aram <aramjzap@gmail.com>
2017-03-13 19:57:11 -0300

    14th code

--- src/sistel/progav/_14.java
+++ src/sistel/progav/_14.java
@@ -1,11 +1,30 @@
-/* Specification here */
+/* Uso de passo variárel no laço 'for' */
 
 /** author: Aram Zap
 /** data: 13/03/17 */
+
 package sistel.progav;
+
+import java.util.Scanner;
 
 public class _14 {
     public static void main(String args[]) {
+        Scanner ler = new Scanner(System.in);
 
+        int inicio, fim, passo;
+
+        System.out.print("Informe o valor da largada: ");
+        inicio = ler.nextInt();
+
+        System.out.print("Informe o valor da chegada: ");
+        fim = ler.nextInt();
+
+        System.out.print("Informe o valor do passo: ");
+        passo = ler.nextInt();
+
+        int i;
+        for (i = inicio; i <= fim; i = i + passo) {
+            System.out.printf("%d - Alô Mundo!!!\n", i);
+        }
     }
 }
