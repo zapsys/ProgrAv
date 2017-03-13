1980faf42691fc0cb9ac46d768646589815408d7
Aram <aramjzap@gmail.com>
2017-03-13 18:46:26 -0300

    Exercício 9

---
+++ src/sistel/progav/_09.java
@@ -0,0 +1,12 @@
+/** author: Aram Zap */
+/** date: 13/03/2017 */
+
+package sistel.progav;
+
+import java.util.Scanner;
+
+public class _09 {
+    public static void main(String args[]) {
+        Scanner ler = new Scanner(System.in);
+    }
+}
