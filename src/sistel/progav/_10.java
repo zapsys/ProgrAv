c17c872e947bcf945d2e3692049ef105dfd69c3d
Aram <aramjzap@gmail.com>
2017-03-13 19:05:22 -0300

    Exercício 10

---
+++ src/sistel/progav/_10.java
@@ -0,0 +1,17 @@
+/* Uso da estrutura de repetição 'for' */
+
+/** author: Aram Zap */
+/** date: 13/03/2017 */
+
+package sistel.progav;
+
+public class _10 {
+    public static void main(String args[]) {
+        int i;
+
+        for (i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+    }
+}
+
