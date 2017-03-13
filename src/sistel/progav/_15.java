f89d77af0f28f3a2ccb7f4dcb6e4756bdb0c6766
Aram <aramjzap@gmail.com>
2017-03-13 20:07:46 -0300

    15th code

---
+++ src/sistel/progav/_15.java
@@ -0,0 +1,15 @@
+/* Passo do laço 'for' com variável do tipo 'double'  */
+
+/** author: Aram Zap
+/** date: 13/03/17 */
+
+package sistel.progav;
+
+public class _15 {
+    public static void main(String args[]) {
+
+        for (double i = 0.25; i <= 2.50; i = i + 0.25) {
+            System.out.printf("%.2f\n", i);
+        }
+    }
+}
