84d6f0436a50cd945a33ee863cbf8bf12fa90f6f
Aram <aramjzap@gmail.com>
2017-03-20 21:09:38 -0300

    21th code

---
+++ src/sistel/progav/_21.java
@@ -0,0 +1,18 @@
+/* Vetores: Especificação de um vetor com número de elementos definido */
+
+/** author: Aram Zap
+ *  date: 20/03/17  */
+
+package sistel.progav;
+
+public class _21 {
+    public static void main(String args[]) {
+        int i;
+        int v[] = {3, 2, 7, 5, 0, -1, 9, -4};
+
+        for (i = 0; i < v.length; i++){
+            System.out.printf("v[%d] = %d\n", i, v[i]);
+        }
+
+    }
+}
