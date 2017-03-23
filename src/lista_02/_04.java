0afbcf25d97c47f1882fc8169bb7499fc4eed8f0
Aram <aramjzap@gmail.com>
2017-03-22 22:41:07 -0300

    Exercício 4

---
+++ src/lista_02/_04.java
@@ -0,0 +1,27 @@
+/* Divisores de um número inteiro */
+
+/** author: Aram Zap */
+/**  date: 20/03/17  */
+
+package lista_02;
+
+import java.util.Scanner;
+
+public class _04 {
+    public static void main(String args[]) {
+        Scanner ler = new Scanner(System.in);
+
+        int n;
+
+        System.out.printf("Entre com um número: ");
+        n = ler.nextInt();
+
+        System.out.print("Divisores: ");
+
+        for (int i = 1; i <= n; i++){
+            if (n % i == 0){
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
