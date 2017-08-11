7e0c15402fe92fcc922be2025569e2575b2a155e
zapsys <zapsyssolutions@gmail.com>
2017-08-11 20:16:25 -0300

    Exercício 1

---
+++ src/Ago_11/Um.java
@@ -0,0 +1,26 @@
+/* Operações básicas entre dois números inteiros */
+package Ago_11;
+
+import java.util.Scanner;
+
+
+public class Um {
+    public static void main(String args[]) {
+        Scanner ler = new Scanner(System.in);
+
+        int a, b;
+
+        System.out.printf("Informe o 1o. valor: ");
+        a = ler.nextInt();
+
+        System.out.printf("Informe o 2o. valor: ");
+        b = ler.nextInt();
+
+        System.out.println();
+
+        System.out.printf("%d + %d = %d\n", a, b, (a+b));
+        System.out.printf("%d - %d = %d\n", a, b, (a-b));
+        System.out.printf("%d * %d = %d\n", a, b, (a*b));
+        System.out.printf("%d / %d = %.2f\n", a, b, (a/(double)(b)));
+    }
+}
