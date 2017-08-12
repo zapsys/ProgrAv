d9c2ebbe752801431102de62604e6af26c3fd5a5
zapsys <zapsyssolutions@gmail.com>
2017-08-11 23:37:33 -0300

    Exercício 4

---
+++ src/Ago_11/Quatro.java
@@ -0,0 +1,12 @@
+/* Specification here */
+package Ago_11;
+
+public class Quatro {
+
+    public static void main(String args[]) {
+        for (int i = 1; i <= 0; i++){
+            System.out.printf("%d - Bom final de semana!!!\n", i);
+        }
+
+    }
+}
