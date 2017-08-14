110905b26eabb9b7e55a036c47c7c4f47a8400ed
zapsys <zapsyssolutions@gmail.com>
2017-08-14 19:40:06 -0300

    Exercício 3

---
+++ src/Ago_14/Tres.java
@@ -0,0 +1,17 @@
+/* Specification here */
+package Ago_14;
+
+public class Tres {
+    public static void main(String args[]) {
+        System.out.printf("\nUsando o comando \"while\":\n");
+
+        int i;
+
+        i = 1;
+        while (i <= 10){
+            System.out.printf("%d\n", i);
+            i++;
+        }
+        System.out.println();
+    }
+}
