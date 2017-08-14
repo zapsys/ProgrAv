743a1cd33d14473b5c5252808c129c1d110c3f57
zapsys <zapsyssolutions@gmail.com>
2017-08-14 20:00:57 -0300

    Exercício 4

---
+++ src/Ago_14/Quatro.java
@@ -0,0 +1,28 @@
+/* Specification here */
+package Ago_14;
+
+public class Quatro {
+    public static void main(String args[]) {
+        System.out.printf("\nUsando o comando \"for\":\n");
+
+        for (int i = 1; i <= 10; i++)
+        {
+            System.out.printf("%d\n", i);
+        }
+
+        System.out.println();
+
+        for (int i = 5; i <= 50; i = i + 5)
+        {
+            System.out.printf("%d\n", i);
+        }
+
+        System.out.println();
+
+        for (int i = 10; i >= 1; i--)
+        {
+            System.out.printf("%d\n", i);
+        }
+
+    }
+}
