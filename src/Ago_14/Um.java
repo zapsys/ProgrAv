d767b2b1e5ad43d638f64a9629e052b3d8795c85
zapsys <zapsyssolutions@gmail.com>
2017-08-18 19:34:48 -0300

    Exercício 1

---
+++ src/Ago_14/Um.java
@@ -0,0 +1,25 @@
+/* Comparação usando 'if' */
+package Ago_14;
+
+import java.util.Scanner;
+
+public class Um {
+    public static void main(String args[]) {
+        Scanner ler = new Scanner(System.in);
+
+        int n;
+
+        System.out.printf("Entre com um número: ");
+        n = ler.nextInt();
+
+        if ((n >= 0) && (n <= 10)) {
+            System.out.printf("O número %d pertence ao intervalo [0,10]\n", n);
+        }
+        if ((n == 5) || (n == 10)) {
+            System.out.printf("O número é %d.\n", n);
+        }
+        if (n == -5) {
+            System.out.printf("O número %d é negativo.\n", n);
+        }
+    }
+}
