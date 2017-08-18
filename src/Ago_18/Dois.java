7263ada532e1b8a134112066387009b08351f829
zapsys <zapsyssolutions@gmail.com>
2017-08-18 19:24:45 -0300

    Exercício 2

---
+++ src/Ago_18/Dois.java
@@ -0,0 +1,24 @@
+/* Fatorial de um número inteiro positivo */
+package Ago_18;
+
+import java.util.Scanner;
+
+public class Dois {
+    public static void main(String args[]) {
+        Scanner ler = new Scanner(System.in);
+
+        int i, n, fatorial;
+
+        System.out.printf("Informe um número inteiro positivo: ");
+        n = ler.nextInt();
+
+        fatorial = 1;
+
+        for(i = 1; i <=n; i++){
+            fatorial = fatorial*i;
+        }
+
+        System.out.printf("%d! = %d\n", n, fatorial);
+
+    }
+}
