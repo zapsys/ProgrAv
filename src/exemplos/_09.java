f6de4c2208b54fab7bc223b24cded1c6b4661cae
zapsys <zapsyssolutions@gmail.com>
2017-08-19 09:30:18 -0300

    Exercício 9

---
+++ src/exemplos/_09.java
@@ -0,0 +1,20 @@
+/* Uso básico da estrutura de repetição 'while' */
+
+/** author: Aram Zap */
+/** date: 13/03/2017 */
+
+package exemplos;
+
+public class _09 {
+    public static void main(String args[]) {
+        int i;
+
+        i = 1;
+
+        while (i <= 10){
+            System.out.printf("%d\n",i);
+
+            i = i + 1;
+        }
+    }
+}
