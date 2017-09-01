79a58a65db5dbf8d6a2b4086ffe75514c69da22e
zapsys <zapsyssolutions@gmail.com>
2017-09-01 19:56:02 -0300

    Exercício 4

---
+++ src/Set_01/Quatro.java
@@ -0,0 +1,23 @@
+/* Specification here */
+package Set_01;
+
+import java.util.Scanner;
+
+public class Quatro {
+    public static void main(String args[]) {
+        Scanner ler = new Scanner(System.in);
+
+        String s;
+        int i, j;
+
+        System.out.printf("Informe uma string: \n");
+        s = ler.nextLine();
+
+        for (i = 0; i < s.length(); i++){
+            for(j = 0; j <= i; j++){
+                System.out.printf("%c ", s.charAt(j));
+            }
+            System.out.println();
+        }
+    }
+}
