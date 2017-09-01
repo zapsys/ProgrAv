903c8dd9df8622b878d009d867acd4103a48ec24
zapsys <zapsyssolutions@gmail.com>
2017-09-01 20:09:21 -0300

    Exercício 5

---
+++ src/Set_01/Cinco.java
@@ -0,0 +1,39 @@
+/* Contagem de caracteres maiúsculos, minúsculos e letras 'A' de um string */
+package Set_01;
+
+
+import java.util.Scanner;
+
+public class Cinco {
+    public static void main(String args[]) {
+        Scanner ler = new Scanner(System.in);
+
+        String s;
+        int i, ctMaius, ctMinus, ctA;
+
+        System.out.printf("Informe uma string: ");
+        s = ler.nextLine();
+
+        ctMaius = 0;
+        ctMinus = 0;
+        ctA     = 0;
+
+        for (i = 0; i < s.length(); i++){
+            if ((s.charAt(i) >= 'A') && (s.charAt(i) <= 'Z'))
+
+                ctMaius = ctMaius + 1;
+
+            if ((s.charAt(i) >= 'a') && (s.charAt(i) <= 'z'))
+
+                ctMinus = ctMinus + 1;
+
+            if ((s.charAt(i) == 'A') || (s.charAt(i) == 'a'))
+
+                ctA = ctA + 1;
+        }
+        System.out.printf("\nMaiúsculas = %d", ctMaius);
+        System.out.printf("\nMinúsculas = %d", ctMinus);
+        System.out.printf("\nLetras 'A' = %d\n", ctA);
+
+    }
+}
