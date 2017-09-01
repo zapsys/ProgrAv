ff794e7dc53488d78eb74e0a729254101f1d2dfc
zapsys <zapsyssolutions@gmail.com>
2017-09-01 19:19:32 -0300

    Exercício 1

---
+++ src/Set_01/Um.java
@@ -0,0 +1,19 @@
+/* Imprimindo strings com laço 'for' */
+package Set_01;
+
+public class Um {
+    public static void main(String args[]) {
+        char c;
+
+        for(c = 'A'; c <= 'Z', c++){
+            System.out.printf("%c",c);
+        }
+
+        System.out.println();
+
+        for (c = 'A'; c <= 'H', c++){
+            System.out.printf("%d - %c\n", (int)c, c);
+        }
+        System.out.println();
+    }
+}
