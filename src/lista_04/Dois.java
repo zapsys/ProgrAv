960dc858db39a4f55b3bcb940e77e5841d8ab560
zapsys <zapsyssolutions@gmail.com>
2017-09-04 20:09:30 -0300

    Exercício 2

---
+++ src/lista_04/Dois.java
@@ -0,0 +1,34 @@
+/* Verifica se uma string possui algarismos numéricos */
+package lista_04;
+
+import java.util.Scanner;
+
+public class Dois {
+    public static void main(String args[]) {
+        Scanner ler = new Scanner(System.in);
+
+        String s, answer;
+        int i;
+        boolean check;
+
+        System.out.printf("Informe uma string: ");
+        s = ler.nextLine();
+
+        check = false;
+
+        i = 0;
+
+        while ((i < s.length()) && (check == false)){
+            if ((s.charAt(i) >= '0') && (s.charAt(i) <= '9'))
+                check = true;
+            else
+                i++;
+        }
+        if (check == true)
+            answer = "A string possui caracteres numéricos";
+        else
+            answer = "A string não possui caracteres numéricos";
+        System.out.println();
+        System.out.printf("%s", answer);
+    }
+}
