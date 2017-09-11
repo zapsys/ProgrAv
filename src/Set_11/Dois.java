302c44e5368bdd12956b54ce811c8b411b2ac7f3
zapsys <zapsyssolutions@gmail.com>
2017-09-11 20:20:39 -0300

    Exercício 2

---
+++ src/Set_11/Dois.java
@@ -0,0 +1,26 @@
+/* Interpretar valor do IMC de uma pessoa a partir do método 'interpretar' da classe 'Geral' */
+package Set_11;
+
+import java.util.Scanner;
+
+public class Dois {
+    public static void main(String args[]) {
+        Scanner ler = new Scanner(System.in);
+
+        double pesoCorporal, altura, vlrIMC;
+
+        System.out.printf("Informe o peso corporal em (kg): ");
+        pesoCorporal = ler.nextDouble();
+
+        System.out.println();
+
+        System.out.printf("Altura (em metros): ");
+        altura = ler.nextDouble();
+
+        vlrIMC = Geral.imc(pesoCorporal, altura);
+
+        System.out.println();
+
+        System.out.printf("IMC: %.2f (%s)\n", vlrIMC, Geral.interpretarIMC(vlrIMC));
+    }
+}
