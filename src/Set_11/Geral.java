228eeb82b78d5efd9d57f81dedab47180fd40f18
zapsys <zapsyssolutions@gmail.com>
2017-09-11 20:23:27 -0300

    Classe Geral

---
+++ src/Set_11/Geral.java
@@ -0,0 +1,48 @@
+package Set_11;
+
+public class Geral {
+    public static int fatorial(int n){
+
+        int i, f = 1;
+
+        for(i = 1; i <= n; i++){
+            f = f * i;
+        }
+        return(f);
+    }
+
+    public static int combinacoes(int n, int p){
+
+        return (fatorial(n) / (fatorial(p) * fatorial(n -p)));
+
+    }
+
+    public static double imc(double pesoCorporal, double altura){
+
+        return(pesoCorporal / Math.pow(altura, 2));
+    }
+
+    public static String interpretarIMC(double vlrIMC){
+        if (vlrIMC < 10.5)
+            return("Baixo peso");
+        else if (vlrIMC < 25.0)
+            return ("Peso adequado");
+        else if (vlrIMC < 30.0)
+            return ("Sobrepeso");
+        else
+            return("Obesidade");
+    }
+
+    public static double divisao(double n, double d){
+
+        if (d != 0)
+            return (n / d);
+        else
+            return (0);
+    }
+
+    public static double percentual(double todo, double parte){
+
+        return (divisao(parte, todo) * 100);
+    }
+}
