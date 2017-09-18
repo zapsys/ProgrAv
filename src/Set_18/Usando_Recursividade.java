7b6adf7ec4d093a5cbe6cc38111fd0e3c5a1cace
zapsys <zapsyssolutions@gmail.com>
2017-09-18 20:07:58 -0300

    Operações com fatorial e vetores com a classe Recursividade

---
+++ src/Set_18/Usando_Recursividade.java
@@ -0,0 +1,18 @@
+/* Usando a classe 'Recursividade' para imprimir o fatorial de números inteiros e vetores */
+package Set_18;
+
+public class Usando_Recursividade {
+
+    public static void main(String args[]) {
+
+        System.out.printf("4! = %d\n", Recursividade.fatorial(4));
+        System.out.printf("7! = %d\n", Recursividade.fatorial(7));
+
+        System.out.println();
+
+        int a[] = {7, 4, 3, -1, 0, 9, 8, 2};
+
+        Recursividade.mostrarVetor(a, 0);
+
+    }
+}
