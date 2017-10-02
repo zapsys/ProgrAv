5b02e468336a0160290c33cc408b312450961fcc
zapsys <zapsyssolutions@gmail.com>
2017-10-02 19:58:11 -0300

    Impressão de valores dos métodos para um quadrado definidos na classe Quadrado

---
+++ src/lista_06/Usa_Quadrado.java
@@ -0,0 +1,22 @@
+/* Specification here */
+package lista_06;
+
+import java.util.Scanner;
+
+public class Usa_Quadrado {
+    public static void main(String args[]) {
+        Scanner ler = new Scanner(System.in);
+
+        int lado;
+
+        System.out.printf("Entre com o lado do quadrado: ");
+        lado = ler.nextInt();
+
+        System.out.println();
+
+        System.out.printf("Área do  quadrado: %.2f\n\n", Quadrado.area(lado));
+        System.out.printf("Perímetro do  quadrado: %.2f\n\n", Quadrado.perimetro(lado));
+        System.out.printf("Diagonal do  quadrado: %.2f\n\n", Quadrado.diagonal(lado));
+
+    }
+}
