62aa8a8d2f285fff0e6777cb32984d5c917dfaa4
zapsys <zapsyssolutions@gmail.com>
2017-10-05 23:21:16 -0300

    Exercício 1

---
+++ src/lista_06/Um.java
@@ -0,0 +1,26 @@
+/* Specification here */
+package lista_06;
+
+public class Um {
+    public static void main(String args[]) {
+
+        int num;
+
+        for (num = 1000; num <= 9999; num++){
+            if (num == (int)Math.pow(verificaNumero(num), 2)){
+                System.out.printf("\n%d\n", num);
+            }
+        }
+
+    }
+    public static int verificaNumero(int num){
+        int dividendo, resto, soma = 0;
+
+        dividendo = num / 100;
+        resto = num % 100;
+
+        soma = soma + dividendo + resto;
+
+        return(soma);
+    }
+}
