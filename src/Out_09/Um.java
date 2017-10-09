90d6e35be63029b46997a812766194131bccca3c
zapsys <zapsyssolutions@gmail.com>
2017-10-09 19:28:33 -0300

    Tratamento de exceções

---
+++ src/Out_09/Um.java
@@ -0,0 +1,37 @@
+/* Tratamento de exceções: 'InputMismatchException' e 'ArithmeticException' */
+package Out_09;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Um {
+    public static void main(String args[]) {
+        Scanner ler = new Scanner(System.in);
+
+        int a, b;
+
+        try {
+            System.out.print("Informe o 1o valor: ");
+            a = ler.nextInt();
+
+            System.out.println();
+
+            System.out.print("Informe o 2o valor: ");
+            b = ler.nextInt();
+
+            System.out.printf("\n%d / %d = %d\n", a, b, divisaoInteira(a, b));
+        } catch (InputMismatchException erro) {
+            System.out.printf("Erro: " + erro);
+        }
+    }
+
+    public static int divisaoInteira(int a, int b) {
+        try {
+            return (a / b);
+        }
+        catch (ArithmeticException erro) {
+            System.out.printf("Erro: " + erro);
+            return(0);
+        }
+    }
+}
