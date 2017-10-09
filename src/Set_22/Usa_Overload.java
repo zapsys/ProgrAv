e035206e1acd5e400a31e0250bfc8370ff86f034
zapsys <zapsyssolutions@gmail.com>
2017-10-09 17:10:57 -0300

    Uso de métodos polimórficos definidos na classe 'Overload'

---
+++ src/Set_22/Usa_Overload.java
@@ -0,0 +1,12 @@
+/* Uso do método 'somaSequencia' a partir da classe 'Overload' */
+
+package Set_22;
+
+public class Usa_Overload {
+    public static void main(String args[]) {
+
+        System.out.printf("Soma =  %d\n\n", Overload.somaSequencia(10));
+        System.out.printf("Soma = %d\n\n", Overload.somaSequencia(2, 10, 2));
+
+    }
+}
