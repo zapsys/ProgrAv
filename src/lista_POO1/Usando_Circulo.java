ce189ddd7cdb216dc0d766c2bf0ed08bb036c9d6
zapsys <zapsyssolutions@gmail.com>
2017-10-27 19:43:43 -0200

    Uso de 'Getters e 'Setters' através do objeto 'x'

---
+++ src/lista_POO1/Usando_Circulo.java
@@ -0,0 +1,26 @@
+/* Uso de métodos definidos em 'Circulo.java' a partir da instanciação do objeto 'x'  */
+package lista_POO1;
+
+import java.util.Scanner;
+
+public class Usando_Circulo {
+    public static void main(String args[]) {
+        Scanner ler = new Scanner(System.in);
+
+        double raio;
+
+        System.out.printf("Informe o valor do raio: ");
+        raio = ler.nextDouble();
+
+        Circulo x = new Circulo(raio);
+
+        System.out.println();
+
+        System.out.printf("Área do círculo: %.2f\n", x.area());
+        System.out.printf("Perímetro do círculo: %.2f\n", x.perimetro());
+        System.out.printf("Diametro do círculo: %.2f\n", x.diametro());
+        System.out.printf("Volume do círculo: %.2f\n", x.volume());
+
+
+    }
+}
