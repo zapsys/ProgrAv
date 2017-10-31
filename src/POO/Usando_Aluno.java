98f30bf00bb349b17823b821a624c917af35b760
zapsys <zapsyssolutions@gmail.com>
2017-10-30 23:17:44 -0200

    Uso da subclasse ou classe filha 'Aluno'

--- src/POO/Usando_Aluno.java
+++ src/POO/Usando_Aluno.java
@@ -4,7 +4,7 @@ package POO;
 public class Usando_Aluno {
     public static void main(String args[]) {
         Aluno x = new Aluno("Maurício dos Santos", 'M', new Data(23, 12, 1995),6,110,7.5,4.5);
-        Aluno y = new Aluno("Karla da Silva", 'F', new Data(07, 8, 1999),28,110,5.5,4.2);
+        Aluno y = new Aluno("Karla da Silva", 'F', new Data(07, 8, 1999),27,110,5.5,4.2);
 
         System.out.print(x.toString());
         System.out.println();
