9ab3f6c7e64b71faa53d8211e53dca49b6752a3d
zapsys <zapsyssolutions@gmail.com>
2017-10-30 23:12:12 -0200

    Classe orientada ao objeto 'Colaborador': extende a classe 'Pessoa.java'

---
+++ src/POO/Colaborador.java
@@ -0,0 +1,57 @@
+package POO;
+
+public class Colaborador extends Pessoa {
+    private int faltasMes;
+    private double salario;
+
+    public Colaborador(String nome, char sexo, Data dtNasc, int faltasMes, double salario) {
+        super(nome, sexo, dtNasc);
+        this.faltasMes = faltasMes;
+        this.salario = salario;
+    }
+
+    public int getFaltasMes() {
+        return faltasMes;
+    }
+
+    public void setFaltasMes(int faltasMes) {
+        this.faltasMes = faltasMes;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    public double valorPremio(){
+        if (faltasMes == 0)
+            return (200.00);
+        else if (faltasMes == 1 || faltasMes == 2)
+            return (100.00);
+        else if  (faltasMes == 3 || faltasMes == 4)
+            return(50.00);
+        else
+            return(0.00);
+    }
+    public double salarioFinal(){
+        return (salario + valorPremio());
+    }
+
+    @Override
+    public String toString() {
+        String result;
+
+        result = String.format("Nome: %s\nSexo: %c\nNascimento: %s\nIdade: %d (anos)\nFaltas (mês): %d\nSalário: %.2f\nPrêmio: R$ %.2f\nSalario Final: R$ %.2f\n",
+                getNome(),
+                getSexo(),
+                getDtNasc().mostrarData(1),
+                getDtNasc().calculaIdade(),
+                getFaltasMes(),
+                salario,
+                valorPremio(),
+                salarioFinal());
+        return result;
+    }
+}
