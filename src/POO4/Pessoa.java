d1d8e846d375ff6fdf91a11d24069b31968f8c8d
zapsys <zapsyssolutions@gmail.com>
2017-11-26 15:30:37 -0200

    Classe pai ou superclasse 'Pessoa.java'

---
+++ src/POO4/Pessoa.java
@@ -0,0 +1,22 @@
+
+package POO4;
+
+
+public class Pessoa {
+    private String nome;
+
+    public Pessoa(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+
+}
