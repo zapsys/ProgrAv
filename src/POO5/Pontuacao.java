c643bc6a6d76bc03f21fa933d86c550f1b45b3d5
zapsys <zapsyssolutions@gmail.com>
2017-11-26 19:14:59 -0200

    Classe java 'Pontuacao.java'

---
+++ src/POO5/Pontuacao.java
@@ -0,0 +1,36 @@
+
+package POO5;
+
+
+public class Pontuacao {
+    private Aluno aluno;
+    private double pontos;
+
+    public Pontuacao(Aluno aluno, double pontos) {
+        this.aluno = aluno;
+        this.pontos = pontos;
+    }
+
+    public Aluno getAluno() {
+        return aluno;
+    }
+
+    public void setAluno(Aluno aluno) {
+        this.aluno = aluno;
+    }
+
+    public double getPontos() {
+        return pontos;
+    }
+
+    public void setPontos(double pontos) {
+        this.pontos = pontos;
+    }
+
+    @Override
+    public String toString() {
+        return (aluno.getNomePessoa()  + " (" + aluno.getSobrenome() + "), " + String.format("%.2f", pontos) + " ");
+    }
+    
+    
+}
