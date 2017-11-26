e022664c9dbcf8c0875b7dc6b1ff5ccf2f17d54a
zapsys <zapsyssolutions@gmail.com>
2017-11-26 12:23:28 -0200

    Exercício 4: Uso do laço 'for'

--- src/Ago_11/Quatro.java
+++ src/Ago_11/Quatro.java
@@ -1,10 +1,11 @@
-/* Specification here */
+/* Escreve 10 linhas com o texto 'Bom final de semana' */
+
 package Ago_11;
 
 public class Quatro {
 
     public static void main(String args[]) {
-        for (int i = 1; i <= 0; i++){
+        for (int i = 1; i <= 10; i++){
             System.out.printf("%d - Bom final de semana!!!\n", i);
         }
 
