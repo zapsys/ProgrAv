ca7b33d5095d34685046abee461eba452120c383
zapsys <zapsyssolutions@gmail.com>
2017-11-26 15:31:44 -0200

    Classe java principal 'Usando_Vaquinha.java'

---
+++ src/POO4/Usando_Vaquinha.java
@@ -0,0 +1,24 @@
+
+package POO4;
+
+import java.util.ArrayList;
+
+
+public class Usando_Vaquinha {
+    public static void main(String[] args) {
+        Vaquinha vaquinha;
+        ArrayList<Contribuicao> contribuicoes = new ArrayList<Contribuicao>();
+        
+        contribuicoes.add(new Contribuicao(new Aluno("aranha","aram@hotmail.com","Aram Zap"),3.50));
+        contribuicoes.add(new Contribuicao(new Aluno("maranha","tony@hotmail.com","Tony Santos"),4.50));
+        contribuicoes.add(new Contribuicao(new Aluno("fabi","fabi@hotmail.com","Fabíola Amaral"),4.60));
+        contribuicoes.add(new Contribuicao(new Aluno("loira","paolaloira@hotmail.com","Paola Santos"),1.50));
+        contribuicoes.add(new Contribuicao(new Aluno("fernandinho","fer85@hotmail.com","Fernando Gonçalves"),2.25));
+        
+        vaquinha = new Vaquinha("Vaquinha Natal 2017", contribuicoes);
+               
+        System.out.println();
+       
+        System.out.printf(vaquinha.mostrar());       
+    }
+}
