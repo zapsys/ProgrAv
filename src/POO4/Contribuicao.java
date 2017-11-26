b4f278917034ebcc7d846045d5332aba24d6dd0a
zapsys <zapsyssolutions@gmail.com>
2017-11-26 15:31:12 -0200

    Classe java 'Contribuicao.java'

---
+++ src/POO4/Contribuicao.java
@@ -0,0 +1,41 @@
+
+package POO4;
+
+
+public class Contribuicao{
+    private Aluno aluno;
+    private double valorContribuicao;
+
+    public Contribuicao(Aluno aluno, double valorContribuicao) {
+        this.aluno = aluno;
+        this.valorContribuicao = valorContribuicao;
+    }
+
+    public Aluno getAluno() {
+        return aluno;
+    }
+
+    public void setAluno(Aluno aluno) {
+        this.aluno = aluno;
+    }
+
+    public double getValorContribuicao() {
+        return valorContribuicao;
+    }
+
+    public void setValorContribuicao(double valorContribuicao) {
+        this.valorContribuicao = valorContribuicao;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return (aluno.getNome()  + " (" + aluno.getApelido() + "), " + String.format("R$ %.2f", valorContribuicao) + "\n");
+    }
+
+    
+
+    
+    
+
+}
