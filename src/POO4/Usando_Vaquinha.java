a401f657eedf09052e319fb8758a42185350ffca
zapsys <zapsyssolutions@gmail.com>
2017-11-26 18:16:47 -0200

    Instanciando objeto da classe 'Vaquinha.java' e ArrayList da classe 'Contribuicao.java'

--- src/POO4/Usando_Vaquinha.java
+++ src/POO4/Usando_Vaquinha.java
@@ -1,4 +1,4 @@
-/* Instanciando a classe 'Vaquinha.java' e ArrayList da classe 'Contribuicao.java' */
+/* Instanciando objeto da classe 'Vaquinha.java' e ArrayList da classe 'Contribuicao.java' */
 
 package POO4;
 
@@ -7,19 +7,17 @@ import java.util.ArrayList;
 
 public class Usando_Vaquinha {
     public static void main(String[] args) {
-        Vaquinha vaquinha;
         ArrayList<Contribuicao> contribuicoes = new ArrayList<Contribuicao>();
+        Vaquinha vaquinha = new Vaquinha("Vaquinha Natal 2017", contribuicoes);
         
-        contribuicoes.add(new Contribuicao(new Aluno("aranha","aram@hotmail.com","Aram Zap"),3.50));
-        contribuicoes.add(new Contribuicao(new Aluno("maranha","tony@hotmail.com","Tony Santos"),4.50));
-        contribuicoes.add(new Contribuicao(new Aluno("fabi","fabi@hotmail.com","Fabíola Amaral"),4.60));
-        contribuicoes.add(new Contribuicao(new Aluno("loira","paolaloira@hotmail.com","Paola Santos"),1.50));
-        contribuicoes.add(new Contribuicao(new Aluno("fernandinho","fer85@hotmail.com","Fernando Gonçalves"),2.25));
-        
-        vaquinha = new Vaquinha("Vaquinha Natal 2017", contribuicoes);
-               
+        vaquinha.adicionar(new Contribuicao(new Aluno("aranha","aram@hotmail.com","Aram Zap"),3.50));
+        vaquinha.adicionar(new Contribuicao(new Aluno("maranha","tony@hotmail.com","Tony Santos"),4.50));
+        vaquinha.adicionar(new Contribuicao(new Aluno("fabi","fabi@hotmail.com","Fabíola Amaral"),4.60));
+        vaquinha.adicionar(new Contribuicao(new Aluno("loira","paolaloira@hotmail.com","Paola Santos"),1.50));
+        vaquinha.adicionar(new Contribuicao(new Aluno("fernandinho","fer85@hotmail.com","Fernando Gonçalves"),2.25));
+
         System.out.println();
        
-        System.out.printf(vaquinha.mostrar());       
+        vaquinha.mostrar();       
     }
 }
