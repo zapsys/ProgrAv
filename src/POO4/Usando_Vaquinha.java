cb78637fef8f3cd3b1f2b9eb147770b96ae001e6
zapsys <zapsyssolutions@gmail.com>
2017-11-27 19:17:00 -0200

    Instanciando ArrayList da classe 'Contribuicao.java' e objeto da classe 'Vaquinha.java'

--- src/POO4/Usando_Vaquinha.java
+++ src/POO4/Usando_Vaquinha.java
@@ -10,7 +10,7 @@ public class Usando_Vaquinha {
         ArrayList<Contribuicao> contribuicoes = new ArrayList<Contribuicao>();
         Vaquinha vaquinha = new Vaquinha("Vaquinha Natal 2017", contribuicoes);
         
-        vaquinha.adicionar(new Contribuicao(new Aluno("aranha","aram@hotmail.com","Aram Zap"),3.50));
+        vaquinha.adicionar(new Contribuicao(new Aluno("aranha","aram@hotmail.com","Aram Zap"),8.50));
         vaquinha.adicionar(new Contribuicao(new Aluno("maranha","tony@hotmail.com","Tony Santos"),4.50));
         vaquinha.adicionar(new Contribuicao(new Aluno("fabi","fabi@hotmail.com","Fabíola Amaral"),4.60));
         vaquinha.adicionar(new Contribuicao(new Aluno("loira","paolaloira@hotmail.com","Paola Santos"),1.50));
