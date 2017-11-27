9c3499a57eed8598919cd8e48deb373855593437
zapsys <zapsyssolutions@gmail.com>
2017-11-27 20:01:29 -0200

    Classe pai ou superclasse 'Pessoa.java'

---
+++ src/POO3/Pessoa.java
@@ -0,0 +1,45 @@
+package POO3;
+
+public class Pessoa {
+    private String nome;
+    private char sexo;
+    private Data dtNasc;
+
+    public Pessoa(String nome, char sexo, Data dtNasc) {
+        this.nome = nome;
+        this.sexo = sexo;
+        this.dtNasc = dtNasc;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public Data getDtNasc() {
+        return dtNasc;
+    }
+
+    public void setDtNasc(Data dtNasc) {
+        this.dtNasc = dtNasc;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome......: " + nome + "\n" +
+                "Sexo......: " + sexo + "\n" +
+                "Nascimento: " + dtNasc.mostrarData(1) + "\n" +
+                "Idade.....: " + dtNasc.calculaIdade() + " anos." ;
+    }
+}
