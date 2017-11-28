f94810f8b430ff45666079adf381a438402cabd0
zapsys <zapsyssolutions@gmail.com>
2017-11-28 00:04:18 -0200

    Exercício 3: Percentual de acadêmicos por sexo usando o método (função) 'percentual' da classe 'Geral'

--- src/Set_11/Tres.java
+++ src/Set_11/Tres.java
@@ -1,10 +1,11 @@
-/* Percentual de acadêmicos de cada sexo usando o método 'percentual' da classe 'Geral' */
+/* Exercício 3: Percentual de acadêmicos por sexo usando o método (função) 'percentual' da classe 'Geral' */
+
 package Set_11;
 
 
 public class Tres {
     public static void main(String args[]) {
-        System.out.printf("%.2f%% de acadêmicas\n", Geral.percentual(18,2));
+        System.out.printf("%.2f%% de acadêmicas\n", Geral.percentual(18, 2));
         System.out.printf("%.2f%% de acadêmicos\n", Geral.percentual(18, 16));
     }
 }
