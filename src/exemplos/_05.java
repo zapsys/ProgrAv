cac5a099fef2d8d0864fb68ea6e4680547a0352c
zapsys <zapsyssolutions@gmail.com>
2017-12-02 00:38:30 -0200

    Exemplo 5: Exemplo uso da estrutura condicional if

---
+++ src/exemplos/_05.java
@@ -0,0 +1,28 @@
+/* Exemplo 5: Exemplo uso da estrutura condicional if */
+
+/** author: Aram Zap */
+/** date: 10/03/2017 */
+
+package exemplos;
+
+import java.util.Scanner;
+
+public class _05 {
+    public static void main(String args[]){
+        Scanner ler = new Scanner(System.in);
+
+        int num;
+
+        System.out.printf("Entre com um numero inteiro: ");
+        num = ler.nextInt();
+
+        if (num > 0) {                                              // if simples
+            System.out.printf("%d eh positivo.\n", num);
+        }
+        if ((num % 2) == 0)                                         // if na forma composta
+            System.out.printf("%d eh par.", num);
+        else
+            System.out.printf("%d eh impar.", num);
+
+    }
+}
