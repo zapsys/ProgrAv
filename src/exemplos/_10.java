03c24e70f268a1d3206ca2160eb0eab590667ddc
zapsys <zapsyssolutions@gmail.com>
2017-12-02 00:41:19 -0200

    Exemplo 10: Uso da estrutura de repetição 'for'

---
+++ src/exemplos/_10.java
@@ -0,0 +1,17 @@
+/* Exemplo 10: Uso da estrutura de repetição 'for' */
+
+/** author: Aram Zap */
+/** date: 13/03/2017 */
+
+package exemplos;
+
+public class _10 {
+    public static void main(String args[]) {
+        int i;
+
+        for (i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+    }
+}
+
