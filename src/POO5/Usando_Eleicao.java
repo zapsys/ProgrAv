23eef302cde57cf6fa5cf9e344f74e506581f0df
zapsys <zapsyssolutions@gmail.com>
2017-12-02 01:49:38 -0200

    Instanciando ArrayList da classe 'Pontuacao.java' e objeto da classe 'Eleicao.java'

--- src/POO5/Usando_Eleicao.java
+++ src/POO5/Usando_Eleicao.java
@@ -12,7 +12,7 @@ public class Usando_Eleicao {
         
         eleicao.adicionar(new Pontuacao(new Aluno("Zap",30,"Aram Zap"),5.50));
         eleicao.adicionar(new Pontuacao(new Aluno("Santos",34,"Tony Santos"),4.50));
-        eleicao.adicionar(new Pontuacao(new Aluno("Oliveira",34,"Bruno"),6.60));
+        eleicao.adicionar(new Pontuacao(new Aluno("Oliveira",31,"Bruno"),6.60));
         eleicao.adicionar(new Pontuacao(new Aluno("dos Passos",23,"Jennifer"),1.50));
         eleicao.adicionar(new Pontuacao(new Aluno("Souza",33,"Fernanda"),6.60));
        
