d66b9f791adb0fb815883d198642327b8c3c435e
zapsys <zapsyssolutions@gmail.com>
2017-12-02 00:40:23 -0200

    Exemplo 8: Avaliação valor maior/menor e diferença entre maior e menor

---
+++ src/exemplos/_08.java
@@ -0,0 +1,31 @@
+/* Exemplo 8: Avaliação valor maior/menor e diferença entre maior e menor */
+
+/** author: Aram Zap */
+/** date: 10/03/2017 */
+
+package exemplos;
+
+import java.util.Scanner;
+
+public class _08 {
+    public static void main(String args[]) {
+        Scanner ler = new Scanner(System.in);
+
+        int a, b, dif;
+
+        System.out.printf("Informe o 1o. valor: ");
+        a = ler.nextInt();
+
+        System.out.printf("Informe o 2o. valor: ");
+        b = ler.nextInt();
+
+        if (a >= b) {
+            dif = a - b;
+            System.out.printf("\n%d - %d = %d\n", a, b, dif);
+        }
+        else {
+            dif = b - a;
+            System.out.printf("\n%d - %d = %d\n", b, a, dif);
+        }
+    }
+}
