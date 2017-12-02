69aecb29613b0ce6e066cb9a088e22f334d06397
zapsys <zapsyssolutions@gmail.com>
2017-12-02 00:47:12 -0200

    Exemplo 18: Vetores: Ler 5 números, armazenar em um vetor e mostrá-los na tela

---
+++ src/exemplos/_18.java
@@ -0,0 +1,28 @@
+/* Exemplo 18: Vetores: Ler 5 números, armazenar em um vetor e mostrá-los na tela */
+
+/** author: Aram Zap
+ *  date: 20/03/17  */
+
+package exemplos;
+
+import java.util.Scanner;
+
+public class _18 {
+    public static void main(String args[]) {
+        Scanner ler = new Scanner(System.in);
+
+        int i, n = 5;
+
+        int v[] = new int[5];
+
+        for (i = 0; i < 5; i++){
+            System.out.printf("Informe o %do elemento de %d: ", (i + 1), n);
+            v[i] = ler.nextInt();
+        }
+        System.out.println();
+
+        for (i = 0; i < n; i++){
+            System.out.printf("v[%d] = %d\n", i, v[i]);
+        }
+    }
+}
