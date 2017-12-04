f401bddc9de45222cdfe6053a0f9ea634ef1f57d
zapsys <zapsyssolutions@gmail.com>
2017-12-04 19:52:16 -0200

    Classe java 'Vaquinha'

--- src/POO4/Vaquinha.java
+++ src/POO4/Vaquinha.java
@@ -9,9 +9,9 @@ public class Vaquinha {
     private String nome;
     ArrayList<Contribuicao> contribuicoes;
 
-    public Vaquinha(String nome, ArrayList<Contribuicao> contribuicoes) {
+    public Vaquinha(String nome) {
         this.nome = nome;
-        this.contribuicoes = contribuicoes;
+        this.contribuicoes = new ArrayList();
     }
         
     public String getNome() {
@@ -74,10 +74,12 @@ public class Vaquinha {
         System.out.print(nome + ":");
         System.out.println();
         System.out.println();
-        System.out.printf(contribuicoes.toString());
-        System.out.println();
+        for (Contribuicao item : contribuicoes){
+            System.out.printf("%s\n",item.toString());
+        }
         System.out.println();
         System.out.printf("Total de contribuiçoes: R$ %.2f",totalContribuicoes);
+        System.out.println();
         System.out.println();
         System.out.printf("Maior contribuição: %s\n", maior().getNome());           
     }
